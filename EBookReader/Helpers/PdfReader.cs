264f0013626db5c1b7fdfabbb572e5b4d3b117d6
Bayr_kf60@itstep.edu.az <Bayr_kf60@itstep.edu.az>
2023-01-13 10:14:34 -0800

    updated

--- EBookReader/Helpers/PdfReader.cs
+++ EBookReader/Helpers/PdfReader.cs
@@ -15,46 +15,46 @@ namespace EBookReader.Helpers
         public static List<Page> GetPages(string path)
         {
             var pages = new List<Page>();
-            ////StringBuilder sb = new StringBuilder();
-            //using(PdfReader reader = new PdfReader(path))
-            //{
-            //    for(int pageNo = 1; pageNo <= reader.NumberOfPages; pageNo++)
-            //    {
-            //        ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
-            //        string text = PdfTextExtractor.GetTextFromPage(reader, pageNo,strategy);
-            //        text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
-            //        pages.Add(new Page
-            //        {
-            //            Content = text,
-            //            PageNo = pageNo,
-            //        });
-            //    }
-            //}
+            //StringBuilder sb = new StringBuilder();
+            using (PdfReader reader = new PdfReader(path))
+            {
+                for (int pageNo = 1; pageNo <= reader.NumberOfPages; pageNo++)
+                {
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string text = PdfTextExtractor.GetTextFromPage(reader, pageNo, strategy);
+                    text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+                    pages.Add(new Page
+                    {
+                        Content = text,
+                        PageNo = pageNo,
+                    });
+                }
+            }
 
 
             // Extracting Image and Text content from Pdf Documents
 
             // open a 128 bit encrypted PDF
-            var pdf = PdfDocument.FromFile(path);
+            //var pdf = PdfDocument.FromFile(path);
 
-            //// Get all text to put in a search index
-            //string text = pdf.ExtractAllText();
+            ////// Get all text to put in a search index
+            ////string text = pdf.ExtractAllText();
 
-            //// Get all Images
-            //var allImages = pdf.ExtractAllImages();
+            ////// Get all Images
+            ////var allImages = pdf.ExtractAllImages();
 
-            // Or even find the precise text and images for each page in the document
-            for (var index = 1; index <= pdf.PageCount; index++)
-            {
-                string text = pdf.ExtractTextFromPage(index);
-                IEnumerable<System.Drawing.Image> images = pdf.ExtractImagesFromPage(index);
-                pages.Add(new Page
-                {
-                    Content = text,
-                    PageNo = index,
-                    Images = images.ToList()
-                });
-            }
+            //// Or even find the precise text and images for each page in the document
+            //for (var index = 1; index <= pdf.PageCount; index++)
+            //{
+            //    string text = pdf.ExtractTextFromPage(index);
+            //    IEnumerable<System.Drawing.Image> images = pdf.ExtractImagesFromPage(index);
+            //    pages.Add(new Page
+            //    {
+            //        Content = text,
+            //        PageNo = index,
+            //        Images = images.ToList()
+            //    });
+            //}
             return pages;
         }
     }
